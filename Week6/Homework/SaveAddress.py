79913ea2a1ea4ce37b0e278464099256330390ae
Tent <Angel_flood@hotmail.com>
2019-12-20 15:37:48 +0700

    Last

--- Week6/Homework/SaveAddress.py
+++ Week6/Homework/SaveAddress.py
@@ -8,10 +8,31 @@ with open("Text/RawAddressData.txt", mode="r",encoding="utf-16") as rf:
         word_sp = list(filter(None, word_sp))
         json_data[i] = {}
         for j in range(len(word_sp)):
-            json_data[i]["No"] =  word_sp[0]
+            if "/" in word_sp[0] or "-" in word_sp[0]:
+                json_data[i]["No"] =  word_sp[0]
 
             if word_sp[-1].isnumeric():
                 json_data[i]["Postcode"] =  word_sp[-1]
+
+            if "ต." in word_sp[j]:
+                json_data[i]["Tumbon"] =  word_sp[j]
+
+            if "อ." in word_sp[j]:
+                json_data[i]["Aumpoar"] =  word_sp[j]
+
+            if "จ." in word_sp[j]:
+                json_data[i]["Country"] =  word_sp[j]
+
+            if "ซ." in word_sp[j]:
+                json_data[i]["Soi"] =  word_sp[j]
+
+            if "ถ." in word_sp[j]:
+                json_data[i]["Road"] =  word_sp[j]
+
+            if "ม." in word_sp[j] or ("หมู่" in word_sp[j] and len(word_sp[j])<= 6):
+                json_data[i]["Mu"] =  word_sp[j]
+
+            #print("---->{}".format(word_sp[j]))
 
     for a in json_data:
         print(json_data[a])
