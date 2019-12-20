da32bdd078a0e83dc52aeca7af5a80f85caf0d6f
Tent <Angel_flood@hotmail.com>
2019-12-20 14:58:03 +0700

    111

--- Week6/Homework/SaveAddress.py
+++ Week6/Homework/SaveAddress.py
@@ -2,14 +2,17 @@ import os.path
 
 with open("Text/RawAddressData.txt", mode="r",encoding="utf-16") as rf:
     addr_data = rf.read().splitlines()
+    json_data = {}
     for i in range(len(addr_data)):
-        word_sp = addr_data[i].replace(" ม."," หมู่ ").split(" ")
+        word_sp = addr_data[i].split(" ")
+        word_sp = list(filter(None, word_sp))
+        json_data[i] = {}
+        for j in range(len(word_sp)):
+            json_data[i]["No"] =  word_sp[0]
 
+            if word_sp[-1].isnumeric():
+                json_data[i]["Postcode"] =  word_sp[-1]
 
-
-        print(word_sp)
-        # for j in word_sp:
-        #     if j == "ม." or j == "หมู่":
-        #         print(word_sp[0])
-
+    for a in json_data:
+        print(json_data[a])
 
