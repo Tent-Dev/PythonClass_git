eb2df54c8ccf918abaea17e7cdd6a17de03c7d88
59122030-6 <Angel_flood@hotmail.com>
2020-01-03 11:05:44 +0700

    last

--- Week7/pymongo2.py
+++ Week7/pymongo2.py
@@ -1,7 +1,6 @@
 import pymongo
 
 def insertData():
-    print(pymongo)
     client = pymongo.MongoClient('localhost', 27017)
     db = client.get_database("TNI")
 
@@ -9,8 +8,40 @@ def insertData():
     #rs = db.students.insert_one({'id':'01111111','name':'Tent'}) #รับข้อมูลเป็น dic ชุดเดียว
 
     rs  = db.students.insert_many([{'id':'02','name':'Tent'},{'id':'03','name':'Tent'}])
-
     for e in rs.inserted_ids:
         print('{}'.format(e))
+
+def selectData():
+    client = pymongo.MongoClient('localhost', 27017)
+    db = client.get_database("MobileShop")
+
+    rs = db.Products.find({'pprice':{'$gt':20000}}).sort([('pprice',pymongo.DESCENDING),('pname',pymongo.ASCENDING)])
+
+    print('Found {} records'.format(rs.count()))
+    j = 1
+    for e in rs:
+        print("{}). {} - {} - {}".format(j, e['pid'], e['pname'], e['pprice']))
+        j+=1
+
+def selectData_choice(min,max):
+    client = pymongo.MongoClient('localhost', 27017)
+    db = client.get_database("MobileShop")
+
+    rs = db.Products.find({'$and':[{'pprice':{'$gte':min}},{'pprice':{'$lte':max}}]}).sort([('pprice',pymongo.DESCENDING),('pname',pymongo.ASCENDING)])
+
+    print('Found {} records'.format(rs.count()))
+    j = 1
+    for e in rs:
+        print("{}). {} - {} - {}".format(j, e['pid'], e['pname'], e['pprice']))
+        j+=1
+
+
 if __name__ == '__main__':
-    insertData()
+
+    get_min = int(input("Input Min value : "))
+    get_max = int(input("Input Max value : "))
+    print("-"*10)
+    selectData_choice(get_min,get_max)
+
+    #insertData()
+    #selectData()
---
+++ Week8/Cloud.py
@@ -0,0 +1,20 @@
+import pymongo
+
+def insertToMongoDbCloud():
+    try:
+        client = pymongo.MongoClient("mongodb+srv://chutipas:TaTent591220306@cluster0-94rnq.mongodb.net/test?retryWrites=true&w=majority")
+        db = client.test
+    except Exception as e:
+        print(e)
+
+    # client = pymongo.MongoClient("mongodb+srv://chutipas:TaTent591220306@cluster0-94rnq.mongodb.net/test?retryWrites=true&w=majority")
+    # db = client.get_database("MobileShop")
+    # rs = db.Products.insert_one({"pid":"p003","pname":"Motion","pbrand":"BlackBerry","pprice":9000,"psize":4.5})
+
+    #print("{}".format(rs.inserted_id))
+    #print("วันนี้ไม่น่ามาเรียนเลย222")
+
+
+if __name__ == '__main__':
+    print("วันนี้ไม่น่ามาเรียนเลย")
+    insertToMongoDbCloud()
